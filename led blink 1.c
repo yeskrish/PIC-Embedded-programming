c2b406a7dfafd2925bc699228597fcdbfe1f24c9
YESVANTHKRISHNA.V <114081669+yeskrish@users.noreply.github.com>
2024-06-13 09:55:42 +0530

    Update led blink 1.c

--- led blink 1.c
+++ led blink 1.c
@@ -1,4 +1,4 @@
-//this program is for to toggle all the pins in the port 
+//this program is for to toggle first pin in the selected port 
 #include<pic.h>
 __CONFIG(0x3f3a);//use this line before converting this code to hex code.
 void delay(unsigned int x)//delay function
@@ -8,10 +8,10 @@ void delay(unsigned int x)//delay function
 void main()
 {
 	TRISB=0x00;//port B has been selected
-	PORTB=0x00;// all the pins of port B
+	PORTB=0x00;// all the pins of port B has been set to low
 	while(1)
 	{
-		PORTB=0x01;
+		PORTB=0x01; //pin 0 has been toggle
 		delay(65000);
 		PORTB=0x00;
 		delay(65000);
